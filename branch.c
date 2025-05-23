a52a66bf9ab36ef441d5ab7938fbf3388c3a9e6a
johnnyleeli <johnnyleeli@tencent.com>
2025-05-23 22:01:58 +0800

    add branch control test

--- branch.c
+++ branch.c
@@ -26,25 +26,45 @@ int branch(int v){
 }
 
 /*
-* gcc branch.c -O3 -o branch.o -c
-* objdump -d branch.o
+* gcc branch.c -g -O3 -o branch.o -c
+* objdump -dS branch.o
+* -O3 优化后的main函数:
+* int main() {printf("what is %d", 9);printf("%d", 1);}
 branch.o:     file format elf64-x86-64
 
 Disassembly of section .text:
 
 0000000000000000 <branch>:
+    i++;
+    printf("%d", i);
+}
+
+int branch(int v){
+    if (v > 16) {
    0:   83 ff 10                cmp    $0x10,%edi
+int branch(int v){
    3:   53                      push   %rbx
    4:   89 fb                   mov    %edi,%ebx
+    if (v > 16) {
    6:   7f 18                   jg     20 <branch+0x20>
+        printf("what is %d", v);
+    }
+
+    const int h = 10;
+    if (h + 1 < 20) {
+        printf("what is %d", v);
    8:   89 de                   mov    %ebx,%esi
    a:   bf 00 00 00 00          mov    $0x0,%edi
    f:   31 c0                   xor    %eax,%eax
   11:   e8 00 00 00 00          callq  16 <branch+0x16>
+    }
+    return 0;
+}
   16:   31 c0                   xor    %eax,%eax
   18:   5b                      pop    %rbx
   19:   c3                      retq
   1a:   66 0f 1f 44 00 00       nopw   0x0(%rax,%rax,1)
+        printf("what is %d", v);
   20:   89 fe                   mov    %edi,%esi
   22:   31 c0                   xor    %eax,%eax
   24:   bf 00 00 00 00          mov    $0x0,%edi
@@ -54,14 +74,19 @@ Disassembly of section .text:
 Disassembly of section .text.startup:
 
 0000000000000000 <main>:
+    if (v > 16) {
    0:   48 83 ec 08             sub    $0x8,%rsp
+int branch(int v){
    4:   be 09 00 00 00          mov    $0x9,%esi
+        printf("what is %d", v);
    9:   bf 00 00 00 00          mov    $0x0,%edi
    e:   31 c0                   xor    %eax,%eax
   10:   e8 00 00 00 00          callq  15 <main+0x15>
   15:   be 01 00 00 00          mov    $0x1,%esi
+}
   1a:   bf 00 00 00 00          mov    $0x0,%edi
   1f:   31 c0                   xor    %eax,%eax
+        printf("what is %d", v);
   21:   48 83 c4 08             add    $0x8,%rsp
   25:   e9 00 00 00 00          jmpq   2a <main+0x2a>
  */
