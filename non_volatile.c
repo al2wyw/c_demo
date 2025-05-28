bc40501595f2f2b1b7005188975541905832a3a4
al2wyw <4485164+al2wyw@users.noreply.github.com>
2025-05-28 22:47:46 +0800

    Update non_volatile.c

--- non_volatile.c
+++ non_volatile.c
@@ -9,10 +9,29 @@
 #include <time.h>
 
 int val = 0;
+// val++; no O 都是一样的
+// movl    16948(%rip), %eax       ## <_val>
+// addl    $1, %eax
+// movl    %eax, 16939(%rip)       ## <_val>
+// val++; O1 都是一样的
+// addl    $1, 16915(%rip)         ## <_val>
+
+// while (!val); no O
+// int val = 0:
+// cmpl    $0, 16792(%rip)      ##  <_val>
+// volatile int val = 0:
+// movl    16793(%rip), %eax    ##  <_val>
+// cmpl    $0, %eax
+// while (!val);  O1
+// int val = 0:
+// 被优化掉了
+// volatile int val = 0:
+// cmpl    $0, 16793(%rip)      ##  <_val>
+// 直接比较imm和内存，从内存读取到reg再比较imm和reg，有什么不同 ???
 
 void *run() {
     sleep(1);
-    printf("%d get value\n", val);
+    printf("%d first get value\n", val);
     val++;
     struct timespec time;
     clock_gettime(CLOCK_REALTIME, &time);
