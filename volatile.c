ed8797f3aafea01ff81312e43672e4369adcd8fd
johnnyleeli <johnnyleeli@tencent.com>
2025-05-29 11:44:50 +0800

    add volatile comments

--- volatile.c
+++ volatile.c
@@ -8,7 +8,8 @@
 // no-nv 和 no-yv 对比，yo-nv 和 yo-yv 对比，调查volatile和编译器优化对val的影响(此时没有mb):
 // no-nv 和 no-yv 的代码基本一致；yo-nv 和 yo-yv的代码不一致，yo-nv把for循环展开了，yo-yv每次从内存读取val
 // yo-yv 和 yo-yf 对比，调查volatile和mb的效果基本一致，val每次从内存读取
-// 使用 volatile 修饰的变量，编译器不会对其进行代码优化，cpu不会将其缓存到寄存器和cpu cache中，保证执行时每次都从内存中读取(防优化、防缓存)
+// 使用 volatile 修饰的变量，编译器不会对其进行代码优化，cpu不会将其缓存到寄存器(但是还会受cpu cache的影响，不过有MESI一致性协议)，保证执行时每次都从内存中读取(防优化、防缓存)
+// c编译器无法感知多线程场景，默认代码逻辑是单线程的
 #include<stdio.h>
 #include<stdlib.h>
 
