cecbd0b5adf4bb1bfde05ad9ca04503f17752897
johnnyleeli <johnnyleeli@tencent.com>
2025-08-19 17:51:47 +0800

    asm opt

--- branch_predict.c
+++ branch_predict.c
@@ -21,6 +21,15 @@ int main() {
     // !!! 排序后下面的Loop运行将更快
     qsort(data, arraySize, sizeof(int), compareInt);
 
+    /*
+    // opt:
+    for (unsigned c = 0; c < arraySize; ++c)
+    {
+        if (data[c] < 128)
+            data[c] = 0;
+    }
+     */
+
     // 测试部分
     long long start = get_timestamp_ms();
     long long sum = 0;
@@ -33,6 +42,13 @@ int main() {
             if (data[c] >= 128)
                 sum += data[c];
         }
+        /*
+        // opt:
+        for (unsigned c = 0; c < arraySize; ++c)
+        {
+            sum += data[c];
+        }
+         */
     }
 
     double elapsedTime = (get_timestamp_ms() - start) / 1000.0;
@@ -40,3 +56,28 @@ int main() {
     printf("%lf\n", elapsedTime);
     printf("%ld\n", sum);
 }
+
+/*
+//使用 %edi 来代替 -40(%rbp), 性能提升20%
+for (unsigned c = 0; c < arraySize; ++c)
+{
+    sum += data[c];
+}
+.LBB5:
+    .loc 1 36 0
+        movl    $0, %edi
+    jmp	.L10
+.L11:
+    .loc 1 38 0 discriminator 2
+    movq	-64(%rbp), %rax
+    movl	%edi, %edx
+    movl	(%rax,%rdx,4), %eax
+    cltq
+    addq	%rax, -32(%rbp)
+    .loc 1 36 0 discriminator 2
+    addl	$1, %edi
+.L10:
+    .loc 1 36 0 is_stmt 0 discriminator 1
+    cmpl	-44(%rbp), %edi
+    jb	.L11
+ */
