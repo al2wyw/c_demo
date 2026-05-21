9dd34526a553d9e5f04ca45b54ae9eb4727758d0
johnnyleeli <johnnyleeli@tencent.com>
2026-05-21 18:55:59 +0800

    context_switch false share cas test

--- concurrent_false_sharing.c
+++ concurrent_false_sharing.c
@@ -7,60 +7,58 @@
 #include <pthread.h>
 #include <string.h>
 
+int NUM = 10;
 typedef struct {
     //int arr_padding1[32];
-    volatile long produce_data;
-    //int arr_padding2[32];
-    volatile long consume_data;
+    volatile long value;
 } data_t;
-data_t shared_data;
+data_t *shared_data;
 long LOOP_COUNT = 200000000;   // 2 亿次，让差异充分体现
 
-// 生产者线程函数
-void* producer(void* arg) {
+void* worker(void* arg) {
+    int index = *(int*)arg;
     for (long i = 0; i < LOOP_COUNT; i++) {
-
-        // 生产数据
-        shared_data.produce_data += i;
-
+        shared_data[index].value += i;
     }
-    printf("producer finished\n");
-    return NULL;
-}
-
-// 消费者线程函数
-void* consumer(void* arg) {
-
-    for (long i = 0; i < LOOP_COUNT; i++) {
-
-        // 消费数据
-        shared_data.consume_data += i;
-
-    }
-    printf("consumer finished\n");
+    printf("worker %d finished\n", index);
     return NULL;
 }
 
 //两个线程并行的跑
 void main(int argc, char *argv[]) {
-    pthread_t producer_thread, consumer_thread;
 
     if (argc > 1) LOOP_COUNT = atol(argv[1]);
+    if (argc > 2) NUM = atoi(argv[2]);
+
+    shared_data = malloc(sizeof(data_t) * NUM);
+    if (shared_data == NULL) {
+        perror("malloc");
+        exit(1);
+    }
 
     printf("LOOP_COUNT=%ld, sizeof(data_t)=%zu, "
-           "&produce_data=%p, &consume_data=%p, distance=%ld bytes\n",
+           "&shared_data[0]=%p, &shared_data[1]=%p, distance=%ld bytes\n",
            LOOP_COUNT, sizeof(data_t),
-           (void*)&shared_data.produce_data,
-           (void*)&shared_data.consume_data,
-           (char*)&shared_data.consume_data - (char*)&shared_data.produce_data);
+           (void*)&shared_data[0],
+           (void*)&shared_data[1],
+           (char*)&shared_data[1] - (char*)&shared_data[0]);
 
-    pthread_create(&producer_thread, NULL, producer, NULL);
-    pthread_create(&consumer_thread, NULL, consumer, NULL);
+
+    pthread_t threads[NUM];
+    int index[NUM];
+    for (int i = 0; i < NUM; i++) {
+        index[i] = i;
+        pthread_create(&threads[i], NULL, worker, &index[i]);
+    }
 
     printf("start\n");
-    pthread_join(producer_thread, NULL);
-    pthread_join(consumer_thread, NULL);
+    for (int i = 0; i < NUM; i++) {
+        pthread_join(threads[i], NULL);
+    }
 
-    printf("done. produce_data=%ld, consume_data=%ld\n",
-           shared_data.produce_data, shared_data.consume_data);
+    for (int i = 0; i < NUM; i++) {
+        printf("done. value=%ld\n", shared_data[i].value);
+    }
+
+    free(shared_data);
 }
