f0cd4078073d5510af9184f680ca3417b43039f7
johnnyleeli <johnnyleeli@tencent.com>
2026-05-31 22:55:21 +0800

    cpu_consistency.c

--- cpu_consistency.c
+++ cpu_consistency.c
@@ -14,15 +14,16 @@ volatile int b = 0;
 volatile int flag = 0;
 
 void* workerA(void* arg) {
-    while (flag == 0);
 
     int count = 0;
     int fail = 0;
     for (int i = 0; i < LOOP_COUNT; i++) {
+        while (flag % 2 == 0); //怎么保证每次循环两个线程都可以同时执行，无论谁先执行完都会等对方
         a = 0;
+        __atomic_thread_fence(__ATOMIC_SEQ_CST);
         a = 1;
         //插入mfence
-        //__atomic_thread_fence(__ATOMIC_SEQ_CST);
+        __atomic_thread_fence(__ATOMIC_SEQ_CST);
         //插入release语义
         //__atomic_thread_fence(__ATOMIC_RELEASE);
         //插入acquire语义
@@ -34,19 +35,21 @@ void* workerA(void* arg) {
         } else {
             fail++;
         }
+        //__atomic_fetch_add(&flag, 1, __ATOMIC_SEQ_CST);
     }
     printf("workerA finished: %d, fail: %d\n", count, fail);
     return NULL;
 }
 
 void* workerB(void* arg) {
-    while (flag == 0);
-
     int count = 0;
     int fail = 0;
     for (int i = 0; i < LOOP_COUNT; i++) {
+        while (flag % 2 == 0);
         b = 0;
+        __atomic_thread_fence(__ATOMIC_SEQ_CST);
         b = 1;
+        __atomic_thread_fence(__ATOMIC_SEQ_CST);
         if (a == 0) {
             count++;
         } else {
@@ -66,6 +69,7 @@ void main(int argc, char *argv[]) {
     for (int i = 0; i < NUM; i++) {
         pthread_create(&threads[i], NULL, i % 2 == 0 ? workerA: workerB, NULL);
     }
+    sleep(1);
     flag = 1;
     for (int i = 0; i < NUM; i++) {
         pthread_join(threads[i], NULL);
