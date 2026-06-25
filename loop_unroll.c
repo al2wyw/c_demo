d8a620ceded199eea58b246f04fbd99372b16fb9
johnnyleeli <johnnyleeli@tencent.com>
2026-06-25 19:35:17 +0800

    loop unroll

--- loop_unroll.c
+++ loop_unroll.c
@@ -10,11 +10,45 @@ long long get_timestamp_ns();
 
 const int LOOP = 100000;
 
-#define OP *
-#define data_type int
+#define OP +
+#define data_type long
+
+int base(data_type* arr, int loop) {
+    long long start = get_timestamp_ns();
+    data_type x = 0x1;
+    for (int i = 0; i < loop; i++) {
+        x = x OP arr[i];
+    }
+    printf("ret:%d, time: %lld\n", x, get_timestamp_ns() - start);
+    return 0;
+}
+
+int test0(data_type* arr, int loop) {
+    long long start = get_timestamp_ns();
+    data_type x = 0x1;
+    for (int i = 0; i < loop; i+=2) {
+        x = x OP arr[i];
+        x = x OP arr[i+1];
+    }
+    printf("ret:%d, time: %lld\n", x, get_timestamp_ns() - start);
+    return 0;
+}
 
 
-int test(data_type* arr, int loop) {
+int test1(data_type* arr, int loop) { // 累积变量变换
+    long long start = get_timestamp_ns();
+    data_type x = 0x1;
+    data_type y = 0x0;
+    for (int i = 0; i < loop; i+=2) {
+        x = x OP arr[i];
+        y = y OP arr[i+1];
+    }
+    x = x OP y;
+    printf("ret:%d, time: %lld\n", x, get_timestamp_ns() - start);
+    return 0;
+}
+
+int test2(data_type* arr, int loop) {
     long long start = get_timestamp_ns();
     data_type x = 0x1;
     for (int i = 0; i < loop; i+=2) {
@@ -25,21 +59,7 @@ int test(data_type* arr, int loop) {
 }
 
 
-int test1(data_type* arr, int loop) { // 累积变量变换
-    long long start = get_timestamp_ns();
-    data_type x = 0x1;
-    data_type y = 0x1;
-    for (int i = 0; i < loop; i+=2) {
-        x = x OP arr[i];
-        y = y OP arr[i+1];
-    }
-    x = x OP y;
-    printf("ret:%d, time: %lld\n", x, get_timestamp_ns() - start);
-    return 0;
-}
-
-
-int test2(data_type* arr, int loop) { // 重新结合变换
+int test3(data_type* arr, int loop) { // 重新结合变换
     long long start = get_timestamp_ns();
     data_type x = 0x1;
     for (int i = 0; i < loop; i+=2) {
@@ -59,9 +79,11 @@ int main(int argc, char *argv[]) {
     for (int i = 0; i < loop; i++) {
         arr[i] = rand() % loop;
     }
-    test(arr, loop);
+    base(arr, loop);
+    test0(arr, loop);
     test1(arr, loop);
     test2(arr, loop);
+    test3(arr, loop);
     free(arr);
     return 0;
 }
