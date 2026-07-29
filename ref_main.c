b96649ae41e3ba33ffe0dbe2292684e6da560e4b
johnnyleeli <johnnyleeli@tencent.com>
2026-07-29 12:08:34 +0800

    ref lib

--- ref_main.c
+++ ref_main.c
@@ -4,7 +4,7 @@
 /*
  * gcc ref_main.c -L ./ -lref -o main
  * ref_val 通过内存偏移地址访问
- * lib_val 也是通过内存偏移地址访问，其GOT的地址指向的是lib_val的内存偏移地址
+ * lib_val 也是通过内存偏移地址访问，其GOT的地址就是其内存偏移地址
  */
 #include "ref.h"
 
